98734599979a29886357bfa936dfe24cad527781
bryanjug <bryandjug@gmail.com>
2021-07-27 19:20:04 -0700

    completed mobile nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,7 +9,14 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
-import style from '../styles/Nav.module.css'
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import styles from '../styles/Nav.module.css'
 
 function HideOnScroll(props) {
   const { children, window } = props;
@@ -34,31 +41,79 @@ HideOnScroll.propTypes = {
   window: PropTypes.func,
 };
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
 export default function HideAppBar(props) {
-  const classes = useStyles();
-    
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+  
   return (
-    <div className={classes.root}>
+    <div className={styles.container}>
         <HideOnScroll {...props}>
-            <AppBar className={style.nav}>
+            <AppBar className={styles.nav}>
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu" className={classes.menuButton}>
-                        <MenuIcon />
+                    <IconButton 
+                    edge="start" 
+                    color="inherit" 
+                    aria-label="menu" 
+                    className={styles.menuButton}
+                    ref={anchorRef}
+                    aria-controls={open ? 'menu-list-grow' : undefined}
+                    aria-haspopup="true"
+                    onClick={handleToggle}
+                    >
+                      <MenuIcon />
                     </IconButton>
-                    <Image src="/logo.png" alt="Netflix logo" width="64" height="20" className={classes.title}/>
-                    <IconButton aria-label="search" color="inherit">
+                    <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                      {({ TransitionProps, placement }) => (
+                        <Grow
+                          className={styles.menuListContainer}
+                          {...TransitionProps}
+                          style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                        >
+                          <Paper>
+                            <ClickAwayListener onClickAway={handleClose}>
+                              <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown} className={styles.menuList}>
+                                <MenuItem onClick={handleClose} className={styles.menuItem}>Home</MenuItem>
+                                <MenuItem onClick={handleClose} className={styles.menuItem}>Play Something</MenuItem>
+                                <MenuItem onClick={handleClose} className={styles.menuItem}>New & Popular</MenuItem>
+                                <MenuItem onClick={handleClose} className={styles.menuItem}>TV Shows</MenuItem>
+                                <MenuItem onClick={handleClose} className={styles.menuItem}>Movies</MenuItem>
+                              </MenuList>
+                            </ClickAwayListener>
+                          </Paper>
+                        </Grow>
+                      )}
+                    </Popper>
+                    <Image src="/logo.png" alt="Netflix logo" width="64" height="20" className={styles.logo}/>
+                    <IconButton aria-label="search" color="inherit" className={styles.searchIcon}>
                         <SearchIcon />
                     </IconButton>
                 </Toolbar>
