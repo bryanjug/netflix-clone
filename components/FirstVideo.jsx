27ed30131ad3b29fc8fa48e9a5579cbf8109fec3
bryanjug <bryandjug@gmail.com>
2021-08-06 20:25:27 -0700

    changed image to background image

--- components/FirstVideo.jsx
+++ components/FirstVideo.jsx
@@ -4,7 +4,11 @@ import styles from '../styles/FirstVideo.module.css'
 const FirstVideo = () => {
     return (
         <div>
-            <Image src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcinemovie.tv%2Fimages%2Fstories%2FMoviePosters%2FAXL_Movie_Poster.jpg&f=1&nofb=1" alt="Netflix logo" layout="responsive" height="100" width="50" />
+            <div className={styles.cover}>
+               <p>Rousing | Exciting | Reality TV | Makeover | Cars</p> 
+               <p>Info</p>
+               <p>Play</p>
+            </div>
         </div>
     )
 }
