500e3ba4f9277a18d3a561ff6ddc4ac1113c5b36
bryanjug <bryandjug@gmail.com>
2021-08-09 20:54:59 -0700

    added text to firstvideo

--- components/FirstVideo.jsx
+++ components/FirstVideo.jsx
@@ -1,13 +1,15 @@
-import Image from 'next/image'
 import styles from '../styles/FirstVideo.module.css'
 
 const FirstVideo = () => {
     return (
         <div className={styles.container}>
             <div className={styles.info}>
-               <p>Rousing | Exciting | Reality TV | Makeover | Cars</p> 
-               <p>Info</p>
-               <p>Play</p>
+                <h1 className={styles.title}>All American</h1>
+                <p className={styles.rank}>#1 in the U.S. Today</p> 
+                <div className={styles.buttons}>
+                        <p>Info</p>
+                        <p>Play</p>
+                </div>
             </div>
         </div>
     )
