7d838942860f4d46d433f9cb0f7a2ea7e0912f2e
bryanjug <bryandjug@gmail.com>
2021-08-10 21:09:10 -0700

    changed nav opacity

--- components/FirstVideo.jsx
+++ components/FirstVideo.jsx
@@ -7,8 +7,8 @@ const FirstVideo = () => {
                 <h1 className={styles.title}>All American</h1>
                 <p className={styles.rank}>#1 in the U.S. Today</p> 
                 <div className={styles.buttons}>
-                        <p>Info</p>
-                        <p>Play</p>
+                    <p>Info</p>
+                    <p>Play</p>
                 </div>
             </div>
         </div>
